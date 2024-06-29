1b851a4b2114f71b679bd7b517bcef59b61b38c9
parthtpujara@gmail.com <ParthtPujara@gmail.com>
2024-06-29 11:02:56 +0530

    category added

--- src/components/core/homePage/Catagories.jsx
+++ src/components/core/homePage/Catagories.jsx
@@ -1,7 +1,30 @@
 import React, { useContext } from 'react'
 import { useEffect } from 'react';
+import { RiArmchairLine } from "react-icons/ri";
+import { GiWoodenChair } from "react-icons/gi";
+import { MdOutlineTableRestaurant } from "react-icons/md";
+
 import { Link } from 'react-router-dom'
+
 const Catagories = () => {
+
+const cat = [
+    {
+        icon: <RiArmchairLine style={{ fontSize: '100px' }} />,
+        name: "Sofa",
+    },
+    {
+        icon: <GiWoodenChair style={{ fontSize: '100px' }} />,
+        name: "chairs",
+    },
+    {
+        icon: <MdOutlineTableRestaurant style={{ fontSize: '100px' }} />,
+        name: "Tables",
+    },
+    
+    
+]
+
     return (
         <div>
             <div className="container-fluid categories px-md-5" style={{ padding: '0 30px' }}>
@@ -9,36 +32,17 @@ const Catagories = () => {
                     <div className="line"></div>
                 </h2>
                 <div className="mt-5 d-flex flex-wrap justify-content-around" style={{ borderRadius: '5px' }}>
-                    <Link to='/sell-laptop'>
+                   {cat.map((item, key) => {
+                    <Link key={key} to={`/${item.name}`}>
                         <div className="card" style={{ border: '0', padding: '15px 5px', minHeight: '200px' }} onClick={() => { localStorage.setItem('deviceId',1) }}>
                             <div className="categories-icon mx-auto pt-3" style={{ marginBottom: '-10px' }}>
-                                <i className="bi bi-laptop" style={{ fontSize: '45px' }}></i>
+                            {item.icon}
                             </div>
                             <div className="card-body text-center">
-                                <h5 className="card-title text-center" style={{ margin: '10px 0' }}>Laptop</h5>
+                                <h5 className="card-title text-center" style={{ margin: '10px 0' }}>{item.name}</h5>
                             </div>
                         </div>
-                    </Link>
-                    <Link to='/sell-laptop'>
-                        <div className="card" style={{ border: '0', padding: '15px 5px', minHeight: '200px' }} onClick={() => { localStorage.setItem('deviceId',1) }}>
-                            <div className="categories-icon mx-auto pt-3" style={{ marginBottom: '-10px' }}>
-                                <i className="bi bi-laptop" style={{ fontSize: '45px' }}></i>
-                            </div>
-                            <div className="card-body text-center">
-                                <h5 className="card-title text-center" style={{ margin: '10px 0' }}>Gaming Consoles</h5>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link to='/sell-laptop'>
-                        <div className="card" style={{ border: '0', padding: '15px 5px', minHeight: '200px' }} onClick={() => { localStorage.setItem('deviceId',1) }}>
-                            <div className="categories-icon mx-auto pt-3" style={{ marginBottom: '-10px' }}>
-                                <i className="bi bi-laptop" style={{ fontSize: '45px' }}></i>
-                            </div>
-                            <div className="card-body text-center">
-                                <h5 className="card-title text-center" style={{ margin: '10px 0' }}>Smart Watches</h5>
-                            </div>
-                        </div>
-                    </Link>
+                    </Link>})}
                 </div>
             </div>
         </div>
